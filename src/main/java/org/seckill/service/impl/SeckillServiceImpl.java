f540e41aacbef891bbc4063e24506e81f01c65ac
huang zexin <342756877@qq.com>
2020-02-19 19:47:25 -0500

    add seckillServiceimpl

---
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -0,0 +1,109 @@
+package org.seckill.service.impl;
+
+import org.seckill.dao.SeckillDAO;
+import org.seckill.dao.SuccessKilledDAO;
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.entity.Seckill;
+import org.seckill.entity.SuccessKilled;
+import org.seckill.enums.SeckillStateEnum;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.seckill.exception.SeckillException;
+import org.seckill.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.DigestUtils;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Service
+public class SeckillServiceImpl implements SeckillService {
+
+    private Logger logger= LoggerFactory.getLogger(this.getClass());
+
+    //用于混淆md5
+    private final String salt="sdf23123~324f!*&^%$YIMB";
+
+
+    @Autowired
+    private SeckillDAO seckillDAO;
+
+    @Autowired
+    private SuccessKilledDAO successKilledDAO;
+
+    public List<Seckill> getSeckillList() {
+        return seckillDAO.queryAll(0,4);
+    }
+
+    public Seckill getById(long seckillId) {
+        return seckillDAO.queryById(seckillId);
+    }
+
+    public Exposer exportSeckillUrl(long seckillId) {
+        Seckill seckill = seckillDAO.queryById(seckillId);
+        if(seckill==null){
+            return new Exposer(false,seckillId);
+        }
+        Date startTime = seckill.getStartTime();
+        Date endTime = seckill.getEndTime();
+
+        Date nowTime= new Date();
+        if(nowTime.getTime()<startTime.getTime()||nowTime.getTime()>endTime.getTime()){
+            return new Exposer(false,seckillId,nowTime.getTime(),startTime.getTime(),endTime.getTime());
+        }
+        String md5= getMD5(seckillId);
+        return new Exposer(true,seckillId,md5);
+    }
+
+    private String getMD5(long seckillId){
+        String base = seckillId + "/" + salt;
+        String md5 = DigestUtils.md5DigestAsHex(base.getBytes());
+        return md5;
+    }
+
+    @Transactional
+    /**
+     * 使用注解控制事务方法的优点：
+     * 1.开发团队达成一致目标，明确标注事务方法的编程风格。
+     * 2.保证事务方法的执行时间尽可能的短，不要穿插其他网络操作，RPC/Http请求/
+     * 3.不是所有的方法都需要事务，如只有一条修改操作，只读操作不需要事务控制
+     */
+    public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException, RepeatKillException, SeckillCloseException {
+        if(md5==null||!md5.equals(getMD5(seckillId))){
+            throw  new SeckillException("seckill data rewrite");
+        }
+            //执行购买逻辑
+        Date nowTime = new Date();
+        try {
+            int updateCount = seckillDAO.reduceNumber(seckillId, nowTime);
+            if (updateCount <= 0) {
+                //没有更新到记录，秒杀结束
+                throw new SeckillCloseException("seckill is closed");
+            } else {
+                //记录购买行为
+                int insertCount = successKilledDAO.insertSuccessKilled(seckillId, userPhone);
+                if (insertCount <= 0) {
+                    throw new RepeatKillException("seckill repeated");
+                } else {
+                    //秒杀成功
+                    SuccessKilled successKilled = successKilledDAO.queryByIdWithSeckill(seckillId, userPhone);
+                    return new SeckillExecution(seckillId, SeckillStateEnum.Success, successKilled);
+                }
+            }
+        }catch (SeckillCloseException e1){
+            throw e1;
+        }catch (RepeatKillException e2){
+            throw e2;
+        }
+        catch (Exception e){
+            logger.error(e.getMessage(),e);
+            throw new SecurityException("seckill inner error"+e.getMessage());
+        }
+    }
+}
